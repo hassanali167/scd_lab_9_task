ea5d8ce35c95634ee3a1dc233a3022c12539d01b
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-20 12:21:54 +0500

    adding files

---
+++ src/main/java/com/example/university_management_system/repository/ProfessorRepository.java
@@ -0,0 +1,11 @@
+package com.example.university_management_system.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.university_management_system.model.Professor;
+
+public interface ProfessorRepository extends MongoRepository<Professor, String> {
+    Optional<Professor> findByName(String name);
+}
