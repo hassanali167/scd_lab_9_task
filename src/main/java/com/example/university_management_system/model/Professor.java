1e57a8d6adc4257db84531a7572881812b35919c
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-20 12:21:46 +0500

    adding files

---
+++ src/main/java/com/example/university_management_system/model/Professor.java
@@ -0,0 +1,48 @@
+package com.example.university_management_system.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "professors")
+public class Professor {
+    @Id
+    private String id;
+    private String name;
+
+    @DBRef
+    private List<Course> courses;
+
+    public Professor() {}
+
+    public Professor(String name, List<Course> courses) {
+        this.name = name;
+        this.courses = courses;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+}
