b3a2badb1e70e9552d99cb61e3f3d23045ed8a21
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-20 12:21:36 +0500

    adding files

---
+++ src/main/java/com/example/university_management_system/model/Course.java
@@ -0,0 +1,33 @@
+package com.example.university_management_system.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "courses")
+public class Course {
+    @Id
+    private String id;
+    private String name;
+
+    public Course() {}
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
